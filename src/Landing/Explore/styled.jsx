0ce9570dcb2787ad397ecfaf8fa84554995a3d19
dayfine <difancpa@gmail.com>
2017-12-26 15:01:39 -0500

    explore

--- src/Landing/Explore/styled.jsx
+++ src/Landing/Explore/styled.jsx
@@ -1,27 +1,38 @@
 import styled from 'styled-components'
 import { Image } from '../../common'
 
-export const Content = styled.div`
-  position: relative;
-  padding: 24px 120px 24px 0px;
+export const Text = styled.a`
+  text-decoration: none;
+  line-height: 14px;
+  font-size: 14px;
+
+  @media screen and (min-width: 768px) {
+    line-height: 1;
+    font-size: 1em;
+  }
+`
+
+export const TextContainer = styled.div`
+  box-sizing: border-box;
+  width: 100%;
+  padding: 12px;
+
   background: #fff;
   border: 1px solid rgba(72, 72, 72, .2);
-  box-sizing: border-box;
+  border-top: none;
   box-shadow: 0px 2px 4px rgba(72, 72, 72, .1);
-  border-radius: 4px;
-  max-width: 30%;
-  min-width: 300px
-`
-export const Text = styled.a`
-  line-height: 1;
-  font-size: 1em;
-  color: #383838;
-  text-decoration: none;
-  margin-left: 112px
+  border-radius: 0 0 4px 4px;
+
+  @media screen and (min-width: 768px) {
+    padding: 24px 12px;
+    border-left: none;
+    border-radius: 0 4px 4px 0;
+  }
 `
 
 export const Picture = Image.extend`
   border-radius: 4px 4px 0 0;
+  box-shadow: 0px 2px 4px rgba(72, 72, 72, .1);
 
   @media screen and (min-width: 768px) {
     width: 96px;
@@ -29,6 +40,7 @@ export const Picture = Image.extend`
     border-radius: 4px 0 0 4px;
   }
 `
+
 export const FlexContainer = styled.div`
   display: flex;
   flex-direction: column;
--- src/common/index.js
+++ src/common/index.js
@@ -1,6 +1,23 @@
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+import Heading2 from './Heading2'
 
 export const Image = styled.img`
   width: 100%;
   object-fit: cover;
 `
+
+export const Card = styled(Link)`
+  display: block;
+  text-decoration: none;
+  color: #383838;
+`
+
+export const CardsContainer = styled.div`
+  position: relative;
+  overflow-x: scroll;
+  margin: 0 -8px;
+  box-sizing: border-bosx;
+`
+
+export { Heading2 }
