59977de2244ba04fde37192c80faf2b6d2caaa8a
dayfine <difancpa@gmail.com>
2017-12-27 15:03:50 -0500

    first deploy

---
+++ server/index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const cors = require('cors')
+const path = require('path')
+// const db = require('./db')
+const port = process.env.PORT || 3001
+let indexPath
+
+if (process.env.NODE_ENV !== 'production') {
+  app.use('/assets', express.static(path.join(__dirname, '../public/assets')))
+  indexPath = path.join(__dirname, '..', 'public', 'index.html')
+} else {
+  app.use(express.static(path.join(__dirname, '..', 'build')))
+  app.use('/assets', express.static(path.join(__dirname, '../build/assets')))
+  indexPath = path.join(__dirname, '..', 'build', 'index.html')
+}
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(morgan('dev'))
+app.use(cors())
+app.use('/public', express.static(path.join(__dirname, '../public')))
+
+app.get('*', (req, res, next) => res.sendFile(indexPath))
+
+app.use((req, res, next) => {
+  const error = new Error('page not found')
+  error.status = 404
+  next(error)
+})
+
+app.listen(port, () => console.log(`listening on port ${port}`))
